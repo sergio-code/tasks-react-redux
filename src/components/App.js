bd4c0f8c7be61f1c950ba7f0e8fce3fef3c166bc
mrgromov <mailrxtx@gmail.com>
2019-07-03 16:41:49 +0400

    from BrowserRouter to Router for using ext history

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import Header from './Header'
 import TasksPage from './TasksPage'
 import LoginPage from './LoginPage'
