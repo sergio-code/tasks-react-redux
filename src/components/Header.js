cdcf43f16e41c7d3d4d21de92e58149c8c6b5a9b
mrgromov <mailrxtx@gmail.com>
2019-07-03 17:57:05 +0400

    changing look in accordance with url

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,59 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signOut } from '../actions'
 
-const Header = ({ signOut, username, isLoggedIn }) => (
-	<header className="header">
-		<nav className="navbar" aria-label="Site navigation bar">
-			<Link className="navbar-brand" to="/">
-				Tasks
+const renderButton = ({ isLoggedIn, signOut, location }) => {
+	console.log(location)
+	if (location.pathname === '/login') {
+		return null
+	}
+	if (isLoggedIn) {
+		return (
+			<button onClick={signOut} className="navbar-login-button">
+				SignOut
+			</button>
+		)
+	}
+	if (!isLoggedIn) {
+		return (
+			<Link className="navbar-login-button" to="/login">
+				SignIn
 			</Link>
-			<div className="navbar-login">
-				{isLoggedIn ? (
-					<span className="navbar-login-info">Hello, {username}</span>
-				) : null}
-				{isLoggedIn ? (
-					<button onClick={signOut} className="navbar-login-button">
-						SignOut
-					</button>
-				) : (
-					<Link className="navbar-login-button" to="/login">
-						SignIn
-					</Link>
-				)}
-			</div>
-		</nav>
-	</header>
-)
+		)
+	}
+}
+
+const Header = (props) => {
+	console.log(props)
+	const { signOut, username, isLoggedIn, location } = props
+	return (
+		<header className="header">
+			<nav className="navbar" aria-label="Site navigation bar">
+				<Link className="navbar-brand" to="/">
+					Tasks
+				</Link>
+				<div className="navbar-login">
+					{isLoggedIn ? (
+						<span className="navbar-login-info">
+							Hello, {username}
+						</span>
+					) : null}
+					{renderButton({ isLoggedIn, signOut, location })}
+				</div>
+			</nav>
+		</header>
+	)
+}
 
 const mapStateToProps = (state) => {
 	const { username, isLoggedIn } = state.auth
 	return { username, isLoggedIn }
 }
 
-export default connect(
-	mapStateToProps,
-	{ signOut }
-)(Header)
+export default withRouter(
+	connect(
+		mapStateToProps,
+		{ signOut }
+	)(Header)
+)
