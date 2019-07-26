ef2cdfbf3c7d9e92707343c683a5d7a3d132285c
mrgromov <mailrxtx@gmail.com>
2019-07-26 07:35:00 +0400

    switched from connect to hooks, get rid of refs

--- src/components/TaskSorting.js
+++ src/components/TaskSorting.js
@@ -1,41 +1,35 @@
-import React, { useRef, useState } from 'react'
-import { connect } from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { setNavigation } from '../actions'
 
-const TaskSorting = ({ setNavigation, sortField, sortDirection }) => {
-	const [showButton, setShowButton] = useState(false)
-	const formRef = useRef(null)
+const TaskSorting = () => {
+	const dispatch = useDispatch()
+	const { sortField, sortDirection } = useSelector((state) => {
+		return { ...state.navigation }
+	})
+	const [field, setField] = useState(sortField)
+	const [direction, setDirection] = useState(sortDirection)
 
 	const handleFormSubmit = (e) => {
 		e.preventDefault()
-		setNavigation({
-			sortField: e.target['sortField'].value,
-			sortDirection: e.target['sortDirection'].value,
-			currentPage: 1
-		})
-		setShowButton(false)
-	}
-
-	const handleSelectChange = () => {
-		if (
-			formRef.current.sortField.value === sortField &&
-			formRef.current.sortDirection.value === sortDirection
-		) {
-			setShowButton(false)
-		} else {
-			setShowButton(true)
-		}
+		dispatch(
+			setNavigation({
+				sortField: field,
+				sortDirection: direction,
+				currentPage: 1
+			})
+		)
 	}
 
 	return (
 		<div className="tasks-sorting">
-			<form onSubmit={handleFormSubmit} ref={formRef}>
+			<form onSubmit={handleFormSubmit}>
 				<label>
 					Sort by{' '}
 					<select
 						name="sortField"
 						defaultValue={sortField}
-						onChange={handleSelectChange}
+						onChange={(e) => setField(e.target.value)}
 					>
 						<option value="email">Email</option>
 						{/* <option value="id">ID</option> */}
@@ -46,23 +40,17 @@ const TaskSorting = ({ setNavigation, sortField, sortDirection }) => {
 				<select
 					name="sortDirection"
 					defaultValue={sortDirection}
-					onChange={handleSelectChange}
+					onChange={(e) => setDirection(e.target.value)}
 				>
 					<option value="desc">Desc &darr;</option>
 					<option value="asc">Asc &uarr;</option>
 				</select>{' '}
-				{showButton && <button type="submit">Apply</button>}
+				{!(field === sortField && direction === sortDirection) && (
+					<button type="submit">Apply</button>
+				)}
 			</form>
 		</div>
 	)
 }
 
-const masStateToProps = (state) => {
-	const { sortField, sortDirection } = state.navigation
-	return { sortField, sortDirection }
-}
-
-export default connect(
-	masStateToProps,
-	{ setNavigation }
-)(TaskSorting)
+export default TaskSorting
